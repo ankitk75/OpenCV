49c2e9d29c42081adb1a405740d5c7a6d93b059e
Ankit <116819905+flyaltair@users.noreply.github.com>
2023-09-20 11:33:00 +0530

    Add files via upload

--- CV Lab/Lab 5/SIFT.py
+++ CV Lab/Lab 5/SIFT.py
@@ -1,23 +1,25 @@
-import cv2 as cv
+import cv2
+import matplotlib.pyplot as plt
+# %matplotlib inline
 
-# Important NOTE: Use opencv <= 3.4.2.16 as
-# SIFT is no longer available in
-# opencv > 3.4.2.16
-# import cv2
+#reading image
+img1 = cv2.imread('eiffel_2.jfif')
+img2 = cv2.imread('eiffel_3.webp')
 
-# Loading the image
-img = cv.imread('../pic/1.jpg')
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-# Converting image to grayscale
-gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+#sift
+sift = cv2.xfeatures2d.SIFT_create()
 
-# Applying SIFT detector
-sift = cv.SIFT_create()
-kp = sift.detect(gray, None)
+keypoints_1, descriptors_1 = sift.detectAndCompute(img1,None)
+keypoints_2, descriptors_2 = sift.detectAndCompute(img2,None)
 
-# Marking the keypoint on the image using circles
-img=cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+#feature matching
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 
-# cv.imwrite('image-with-keypoints.jpg', img)
-cv.imshow('image=with-keypoints', img)
-cv.waitKey(0)
+matches = bf.match(descriptors_1,descriptors_2)
+matches = sorted(matches, key = lambda x:x.distance)
+
+img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2, matches[:50], img2, flags=2)
+plt.imshow(img3),plt.show()mg_1)
